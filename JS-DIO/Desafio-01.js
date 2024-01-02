cc93095d1e70cc7e247c5b2878b4551cb4c1ac4b
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2024-01-02 18:44:46 -0300

    Delete JS-DIO directory

--- JS-DIO/Desafio-01.js
+++
@@ -1,7 +0,0 @@
-const ValorCombustivel = 5.79;
-const GastoMedioCombustivel = 12;
-const DistanciaemKm = 1580;
-
-let valorGasto = DistanciaemKm / GastoMedioCombustivel * ValorCombustivel;
-
-console.log('O valor gasto de combustivel será:' + valorGasto.toFixed(2));
--- JS-DIO/DesafioDeFunction-01.js
+++
@@ -1,9 +0,0 @@
-function escrevaMeuNome(nome){
-    console.log('Meu nome é: ' + nome);
-}
-
-(function () {
-    escrevaMeuNome('Renan');
-    escrevaMeuNome('Soraia');
-}
-)();
